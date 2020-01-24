eab64789b732fe6383f05c450e6ce996af2bb2e1
LexuanLiu123 <lel006@ucsd.edu>
2020-01-23 17:35:09 -0800

    Initial Commit

---
+++ tippy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tippy
+//
+//  Created by Judy Liu on 2020-01-23.
+//  Copyright © 2020 Judy Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
